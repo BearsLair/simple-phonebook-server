3e9d6220e84f84cfb4b4b2165788d655e67f6389
Patrick Gross <bears_lair@protonmail.com>
2025-02-06 20:53:17 -0800

    post request with error handling now working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const PORT = 3000;
 
 // JSON list of persons in phonebook, present in code for learning purposes
@@ -66,6 +67,40 @@ app.delete("/api/phonebook/:personId", (request, response) => {
   phonebook.splice(deleteIndex, 1);
 
   response.send("Entry deleted");
+});
+
+// Crude method of generating id's, but works for the time being
+const generateId = () => {
+  return Math.floor(Math.random() * 1000);
+};
+
+// Add single entry
+app.post("/api/phonebook", (request, response) => {
+  const addRequest = request.body;
+
+  // If name or number is not in the request
+  if (!("name" in addRequest)) {
+    return response.status(400).send("missing name");
+  } else if (!("number" in addRequest)) {
+    return response.status(400).send("missing number");
+  }
+
+  // Section of code handles name duplicate error
+  let repeatedNameCount = 0;
+  phonebook.map((person) => {
+    person.name === addRequest.name ? repeatedNameCount++ : null;
+  });
+
+  console.log(repeatedNameCount);
+
+  if (repeatedNameCount > 0) {
+    return response.status(400).send("person's name already exists");
+  }
+
+  const id = generateId();
+  const newPerson = { id: `${id}`, ...addRequest };
+  phonebook.push(newPerson);
+  response.send("New entry posted");
 });
 
 app.listen(PORT, () => {
